e0a7880a486c8fe5abbdcf9b75d6526b513448ee
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-04 18:20:16 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-pipe-transform-data-demo';
+  happynewyear: Date = new Date(2023, 0, 1);
+  name: string = 'aissam';
+  salary: number = 1500.5;
+  largeNumber: number = 12345.12345;
+  decimal: number = 0.85;
 }
